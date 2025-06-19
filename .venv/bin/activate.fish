173e8b8c7839f5d9f2479c04f5125e3c4af9dc6e
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2025-06-19 11:30:39 -0300

    sacando el codigo del entorno

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/usuario/Desktop/FP/Renaults_Cup/.venv
+set -gx VIRTUAL_ENV /home/usuario/Desktop/FP/Renault_Cup/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
